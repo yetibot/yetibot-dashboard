0c924f8bc4c06e80235abdd49fdab32118571e59
Trevor Hartman <trevorhartman@gmail.com>
2018-05-18 11:54:47 -0600

    Fill in the Dashboard with real values

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,6 +13,7 @@ import {
   NavbarBrand,
   NavbarItem } from 'bloomer';
 import { Adapters } from './Adapters';
+import { Repl } from './Repl';
 import { History } from './History';
 import { Dashboard } from './Dashboard';
 import YetibotLogo from '-!svg-react-loader?name=yetibotLogo!../img/yetibot_lambda_blue_with_grey.svg';
@@ -21,6 +22,7 @@ import {
   BrowserRouter as Router,
   Route,
   Link,
+  NavLink,
   Switch,
   Redirect
 } from 'react-router-dom'
@@ -38,55 +40,67 @@ interface Props {
 }
 
 export class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    NavLink.defaultProps = {...NavLink.defaultProps, activeClassName: 'is-active'};
+    this.state = {timezoneOffsetHours: new Date().getTimezoneOffset() / 60}
+  }
+
   render () {
     return (
       <ApolloProvider client={client}>
         <Router>
           <div>
 
-            <Navbar className='is-black' style={{marginBottom: 20}}>
-              <NavbarBrand>
-                <NavbarItem>
-                  <Link to='/'>
-                    <YetibotLogo style={{width: 200, height: 28}} />
-                  </Link>
-                </NavbarItem>
-              </NavbarBrand>
+            <Navbar className='is-white'>
+              <Container>
+                <NavbarBrand>
+                  <NavbarItem>
+                    <NavLink activeClassName="is-active" to='/'>
+                      <YetibotLogo style={{width: 120, height: 28}} />
+                    </NavLink>
+                  </NavbarItem>
+                </NavbarBrand>
+              </Container>
             </Navbar>
 
-            <Container className='is-fluid'>
+            <Container className=''>
               <div className='columns'>
-                <div className='column is-3'>
+                <div className='column is-2'>
                   <Menu>
                     <MenuLabel>Yetibot</MenuLabel>
                     <MenuList>
-                      <li><Link to="/">Dashboard</Link></li>
-                      <li><Link to="/history">History</Link></li>
-                      <li><Link to="/adapters">Adapters</Link></li>
-                      <li><Link to="/users">Users</Link></li>
-                      <li><Link to="/repl">REPL</Link></li>
+                      <li><NavLink exact={true} to='/'>Dashboard</NavLink></li>
+                      <li><NavLink to='/history'>History</NavLink></li>
+                      <li><NavLink to='/adapters'>Adapters</NavLink></li>
+                      <li><NavLink to='/users'>Users</NavLink></li>
+                      <li><NavLink to='/repl'>REPL</NavLink></li>
                     </MenuList>
 
                     <MenuLabel>Administration</MenuLabel>
                     <MenuList>
-                      <li><Link to="history">Configuration</Link></li>
-                      <li><Link to="history">Permissions</Link></li>
+                      <li><Link to='history'>Configuration</Link></li>
+                      <li><Link to='history'>Permissions</Link></li>
                     </MenuList>
 
                     <MenuLabel>Links</MenuLabel>
                     <MenuList>
-                      <li><Link to="history">GitHub</Link></li>
-                      <li><Link to="history">Blog</Link></li>
-                      <li><Link to="history">Docs</Link></li>
+                      <li><Link to='history'>GitHub</Link></li>
+                      <li><Link to='history'>Blog</Link></li>
+                      <li><Link to='history'>Docs</Link></li>
                     </MenuList>
                   </Menu>
 
                 </div>
 
                 <div className='column is-9'>
-                  <Route path='/' exact={true} component={Dashboard} />
+                  <Route path='/' exact={true} render={() =>
+                    <Dashboard timezoneOffsetHours={this.state.timezoneOffsetHours} />
+                  } />
                   <Route path='/adapters' component={Adapters} />
                   <Route path='/history' component={History} />
+                  <Route path='/repl' component={Repl} />
                 </div>
 
               </div>
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,31 +1,107 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
-import { Hero, HeroBody, Title } from 'bloomer';
+import {Tile, Hero, HeroBody, Title, Subtitle, Box} from 'bloomer';
 
 const DASHBOARD = gql`
-  {
-    adapters {
-      platform
-      uuid
+  query stats($timezone_offset_hours: Int!) {
+    stats(timezone_offset_hours: $timezone_offset_hours) {
+      uptime
+      adapters
+      users
+      command_count_today
+      command_count
+      history_count
+      history_count_today
     }
   }
 `;
 
-export const Dashboard = () => (
-  <Query query={DASHBOARD}>
+export const Dashboard = ({timezoneOffsetHours}) => (
+  <Query query={DASHBOARD} variables={{timezone_offset_hours: timezoneOffsetHours}}>
     {({ loading, error, data }) => {
       if (loading) return <p>Loading...</p>;
       if (error) return <p>Error {error}</p>;
+      const stats = data.stats;
       return (
         <div>
           <Hero isBold isColor='info' isSize='small'>
             <HeroBody>
-              <Title>
-                Yetibot Dashboard
-              </Title>
+              <Title>Dashboard</Title>
+              <Subtitle>Uptime {stats.uptime}</Subtitle>
             </HeroBody>
           </Hero>
+
+          <div className='tiles'>
+            <Tile isAncestor hasTextAlign='center'>
+
+                <Tile isSize={4} isParent>
+                  <Tile isChild className='box'>
+                    <Title>{stats.adapters}</Title>
+                    <Subtitle>Adapters</Subtitle>
+                  </Tile>
+                </Tile>
+
+                <Tile isSize={4} isParent>
+                  <Tile isChild className='box'>
+                    <Title>{stats.command_count}</Title>
+                    <Subtitle>Commands</Subtitle>
+                  </Tile>
+                </Tile>
+
+                <Tile isSize={4} isParent>
+                  <Tile isChild className='box'>
+                    <Title>{stats.command_count_today}</Title>
+                    <Subtitle>Commands today</Subtitle>
+                  </Tile>
+                </Tile>
+
+                <Tile isSize={4} isParent>
+                  <Tile isChild className='box'>
+                    <Title>{stats.users}</Title>
+                    <Subtitle>Users</Subtitle>
+                  </Tile>
+                </Tile>
+
+
+                <Tile isSize={4} isParent>
+                  <Tile isChild className='box'>
+                    <Title>{stats.history_count}</Title>
+                    <Subtitle>History items</Subtitle>
+                  </Tile>
+                </Tile>
+
+                <Tile isSize={4} isParent>
+                  <Tile isChild className='box'>
+                    <Title>{stats.history_count_today}</Title>
+                    <Subtitle>History items today</Subtitle>
+                  </Tile>
+                </Tile>
+
+                {/* These need a different way to be displayed - maybe charts
+                * Other ideas:
+                * - Number of Errors
+                * - Average Yetibot response time
+                */}
+
+                {/* <Tile isSize={4} isParent> */}
+                {/*   <Tile isChild className='box'> */}
+                {/*     <Title>34 / 55</Title> */}
+                {/*     <Subtitle>Configured commmands / available commands</Subtitle> */}
+                {/*   </Tile> */}
+                {/* </Tile> */}
+
+                {/* <Tile isSize={4} isParent> */}
+                {/*   <Tile isChild className='box'> */}
+                {/*     <Title>Top 3 Commands</Title> */}
+                {/*     <Subtitle>Foo bar</Subtitle> */}
+                {/*   </Tile> */}
+                {/* </Tile> */}
+
+
+
+            </Tile>
+          </div>
 
         </div>
       );
